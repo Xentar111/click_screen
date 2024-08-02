1f53decb0e8a513476f23ffb694acc6b85f9c297
[Carlos Matias] <xentar111@gmail.com>
2024-08-02 20:46:25 -0300

    find template on screen

--- open_cv_version.py
+++ open_cv_version.py
@@ -9,6 +9,11 @@ def capture_screenshoot(region=None):
     screenshoot = cv2.cvtColor(np.array(screenshoot), cv2.COLOR_BGR2RGB)
     return screenshoot
 
+def find_template_on_screen(template_path, threshold=0.8):
+    template = cv2.imread(template_path, 0)
+    print(template.shape)
+    w, h = template.shape[::-1]
+
 def click_on_screen(x, y):
     pyautogui.moveTo(x, y)
     pyautogui.click()
@@ -16,3 +21,4 @@ def click_on_screen(x, y):
 if __name__ == "__main__":
     for root, dirs, file in os.walk('./resources_example'):
         #print(root, dirs, file)
+        
