35b53adcedd905fd596f84b8d4d5a4a7ad9cb752
[Carlos Matias] <xentar111@gmail.com>
2024-08-02 20:43:14 -0300

    click on screen added

--- open_cv_version.py
+++ open_cv_version.py
@@ -9,3 +9,10 @@ def capture_screenshoot(region=None):
     screenshoot = cv2.cvtColor(np.array(screenshoot), cv2.COLOR_BGR2RGB)
     return screenshoot
 
+def click_on_screen(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+if __name__ == "__main__":
+    for root, dirs, file in os.walk('./resources_example'):
+        #print(root, dirs, file)
