8671bd1f5475f046296f1e09a1440b1f65440f1e
[Carlos Matias] <xentar111@gmail.com>
2024-08-02 21:39:44 -0300

    error en capture_screenshoot revisar codigo

--- open_cv_version.py
+++ open_cv_version.py
@@ -12,8 +12,24 @@ def capture_screenshoot(region=None):
 def find_template_on_screen(template_path, threshold=0.8):
     template = cv2.imread(template_path, 0)
     #print(template.shape)
-    w, h = template.shape[::-1]
+    weight, height = template.shape[::-1]#invertir ambos shapes desde imagen
     #print(w, h)
+    
+    
+    screen_shoot = capture_screenshoot()
+    #screenshoot = pyautogui.screenshot(region=region)
+    #screenshoot = cv2.cvtColor(np.array(screenshoot), cv2.COLOR_BGR2RGB)
+    screen_gray = cv2.cvtColor(screen_shoot, cv2.COLOR_BGR2GRAY)
+    print(screen_gray)
+    
+    #encontrar mediante la plantilla el elemento en la pantalla
+    res = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
+    print(res.shape)
+    loc = np.where(res >= threshold)
+    print(loc)
+    
+    
+    
 
 def click_on_screen(x, y):
     pyautogui.moveTo(x, y)
